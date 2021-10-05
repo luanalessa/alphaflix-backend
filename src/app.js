bb1273518d93021e6520ca9121984015061dfc34
Lessa <lessalsn@gmail.com>
2021-10-05 17:08:57 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+
+const app = express();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.json({ type: 'application/vnd.api+json' }));
+// app.use(cookieParser());
+
+// const corsOptions = {
+//     origin: 'http://localhost:8080',
+//     credentials: true,
+//   }
+
+// app.use(cors(corsOptions))
+// app.use(cookieParser());
+
+
+
+
+export default app;
